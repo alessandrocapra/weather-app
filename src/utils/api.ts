0c1f6c182895648ff0cc14f339ca8e6fb92c4db9
Alessandro Capra <alessandro@quantoz.com>
2023-07-08 19:04:41 +0200

    Working WIP with sorting of forecasts

---
+++ src/utils/api.ts
@@ -0,0 +1,50 @@
+type GetCurrentWeatherProps = {
+  city: string | undefined;
+};
+
+type GetForecastProps = {
+  lat: number;
+  lon: number;
+};
+
+export async function getCurrentWeather({
+  city = "Utrecht",
+}: GetCurrentWeatherProps): Promise<CurrentWeatherResponse> {
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=6019bd99eb84923645be2ba229c5a99a`
+    );
+
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error(
+      "An error occurred while fetching the current weather:",
+      error
+    );
+    throw error;
+  }
+}
+
+export async function getForecast({
+  lat,
+  lon,
+}: GetForecastProps): Promise<ForecastResponse> {
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=6019bd99eb84923645be2ba229c5a99a`
+    );
+
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error("An error occurred while fetching the forecast:", error);
+    throw error;
+  }
+}
---
+++ src/utils/api.type.ts
@@ -0,0 +1,100 @@
+type CurrentWeatherResponse = {
+  base: string;
+  clouds: {
+    all: number;
+  };
+  cod: number;
+  coord: {
+    lat: number;
+    lon: number;
+  };
+  dt: number;
+  id: number;
+  main: {
+    feels_like: number;
+    humidity: number;
+    pressure: number;
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  name: string;
+  sys: {
+    country: string;
+    id: number;
+    sunrise: number;
+    sunset: number;
+    type: number;
+  };
+  timezone: number;
+  visibility: number;
+  weather: {
+    description: string;
+    icon: string;
+    id: number;
+    main: string;
+  }[];
+  wind: {
+    deg: number;
+    gust: number;
+    speed: number;
+  };
+};
+
+type ForecastEntry = {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: Array<{
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }>;
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  rain?: {
+    "3h": number;
+  };
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+};
+
+type ForecastResponse = {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: ForecastEntry[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+};
